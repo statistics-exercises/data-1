5fa5c4981d9d8aa2b7f46770b63a74b5ea2a1fcc
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 12:50:48 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ class UnitTests(unittest.TestCase) :
        figdat = fighand.get_lines()[0].get_xydata()
        this_x, this_y = zip(*figdat)
        for i in range( len(this_x) ) :
-           self.assertTrue( np.abs(i+1 - this_x[i])<1e-7 )
+           self.assertTrue( np.abs(i+1 - this_x[i])<1e-7, "One or more of the x-coordinates in your graph are not correct." )
   
     def test_yvalues(self):
        fighand=plt.gca()
@@ -18,4 +18,4 @@ class UnitTests(unittest.TestCase) :
        this_x, this_y = zip(*figdat)
        myy = np.loadtxt("data.dat")
        for i in range( len(myy) ) :
-           self.assertTrue( np.abs(myy[i] - this_y[i])<1e-7 )
+           self.assertTrue( np.abs(myy[i] - this_y[i])<1e-7, "One or more of the y-coordinates in your graph are not correct." )
